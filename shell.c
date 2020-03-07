d78308ab337c7589b272354c8d12cb11d10fd017
yoges <47446121+yogeskc@users.noreply.github.com>
2020-03-06 16:40:30 -0800

    Add files via upload

---
+++ shell.c
@@ -0,0 +1,50 @@
+/**************************************************************
+* Class: CSC-415-03 Spring 2020
+* Name: Yangesh KC
+* Student ID: 920771082
+* Project: <project name, like Assignment 1 – Simple Shell
+*
+* File: Shell.c
+* Description:
+*
+**************************************************************/
+
+void loop(void){
+  //temporary variable
+   char *line;
+   char **args; //array of strings
+   int status;
+
+   //read the commands entered by the user
+   //after input it will parse the commands so computer can understand the commands
+   //execute the commands
+   do{
+        printf("%s","SHELL@root >");
+        line = read();
+        args = parse(line);
+
+        status = execute(args);
+
+         //freeing the memory
+   free(line);
+   free(args);
+
+   }while(status);
+
+
+}
+
+
+int main(int argc, char** argv){
+
+/* code */
+//load the configuration
+
+
+//run an REPL loop
+loop();
+
+//Perform any shutdown cleanup
+return 0;
+
+}
